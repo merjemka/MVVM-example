30582c5e294866af4a9ac4c25135328245d5882a
MerjemKA <MerjemKA@MERJEMKA>
2023-02-13 13:05:04 +0100

    Add project files.

---
+++ WpfApp1/Model/HelloWorldModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    public class HelloWorldModel
+    {
+        //provides data for the ViewModel by pulling it from the repository - GetData method
+        private List<string> repositoryData;
+        public string ImportantInfo
+        {
+            get { return ConcatenateData(repositoryData); }
+        }
+        public HelloWorldModel()
+        {
+            repositoryData = GetData();
+        }
+
+        private List<string> GetData()
+        {
+            repositoryData = new List<string>();
+           
+            return repositoryData;
+        }
+
+        private string ConcatenateData(List<string> dataList) {
+            string importantInfo = dataList.ElementAt(0) + ", " + dataList.ElementAt(1) + "!";
+            return importantInfo;
+        }
+    }
+    
+}
---
+++ WpfApp1/ViewModel/HelloWorldViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    public class HelloWorldViewModel : INotifyPropertyChanged
+    {
+        private string helloString;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string HelloString
+        {
+            get { return helloString; }
+            set
+            {
+                helloString = value;
+                // we call propertychanged in set
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public HelloWorldViewModel ()
+        {
+            HelloWorldModel helloWorldModel= new HelloWorldModel();
+            helloString = helloWorldModel.ImportantInfo;
+        }
+    }
+}
